7fe807c6bcd17a47499e8acd64e6f43b4793a8b3
santhosh <santhoshgrandhi9@gmail.com>
2025-06-01 13:29:47 +0530

    Added Screenshot Method

---
+++ src/test/java/SanthoshFramework/Practice/ScreenshotMethod.java
@@ -0,0 +1,28 @@
+package SanthoshFramework.Practice;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ScreenshotMethod {
+	
+	public static void main(String[] args) throws IOException
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		File src = ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C://Users//Santhosh//eclipse-workspace//Practice//ss.png"));
+		
+		
+	}
+}
