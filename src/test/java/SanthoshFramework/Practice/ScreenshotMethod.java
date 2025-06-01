8b32cb0edbd56df769ecfb31b358f70f4703f442
santhosh <santhoshgrandhi9@gmail.com>
2025-06-01 13:40:24 +0530

    getScreenshot Method

--- src/test/java/SanthoshFramework/Practice/ScreenshotMethod.java
+++ src/test/java/SanthoshFramework/Practice/ScreenshotMethod.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -19,9 +20,14 @@ public class ScreenshotMethod {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
 		
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		File src = ts.getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(src, new File("C://Users//Santhosh//eclipse-workspace//Practice//ss.png"));
+//		TakesScreenshot ts = (TakesScreenshot) driver;
+//		File src = ts.getScreenshotAs(OutputType.FILE);
+//		FileUtils.copyFile(src, new File("C://Users//Santhosh//eclipse-workspace//Practice//ss.png"));
+		
+		
+		File src1 = driver.findElement(By.cssSelector(".lnXdpd")).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src1, new File("C://Users//Santhosh//eclipse-workspace//Practice//ss1.png"));
+		
 		
 		
 	}
