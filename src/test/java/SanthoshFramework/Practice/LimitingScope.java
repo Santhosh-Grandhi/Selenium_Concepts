294557493f7a7b2e8b7ae3303db8afec012baa5b
santhosh <santhoshgrandhi9@gmail.com>
2025-06-02 14:48:22 +0530

    Limiting driver scope code further

--- src/test/java/SanthoshFramework/Practice/LimitingScope.java
+++ src/test/java/SanthoshFramework/Practice/LimitingScope.java
@@ -14,12 +14,13 @@ public class LimitingScope {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
-		
+		//To find number of links present in page footer
 		WebElement driver2 = driver.findElement(By.cssSelector(".navFooterVerticalColumn"));
 		System.out.println(driver2.findElements(By.tagName("a")).size());
 		
-		
-		
+		//To find no.of links present in one column
+		WebElement driver3 = driver.findElement(By.xpath("//div[@class=\"navFooterLinkCol navAccessibility\"][1]"));
+		System.out.println(driver3.findElements(By.tagName("a")).size());
 
 	}
 
